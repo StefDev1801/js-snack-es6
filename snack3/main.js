898057fea4c9b9233488b3794ef7dd37ccdad486
StefDev1801 <stefano200118@gmail.com>
2024-03-05 15:04:59 +0100

    first commit

---
+++ snack3/main.js
@@ -0,0 +1,8 @@
+// Crea un array di oggetti che rappresentano degli animali.
+// Ogni animale ha un nome, una famiglia e una classe.
+// Es:
+// [
+//   { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+//   { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+//   { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+// ]
