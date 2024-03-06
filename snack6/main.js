b01c41f6362140d98f912a9e94ce5fab0003b4a4
StefDev1801 <stefano200118@gmail.com>
2024-03-06 17:43:28 +0100

    add snack 6

--- snack6/main.js
+++ snack6/main.js
@@ -1,5 +1,41 @@
 // Creare un array di oggetti di squadre di calcio.
 // Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+
+const squadreDiCalcio = [
+    { nome: "Juventus", punti: 0, falliSubiti: 0 },
+    { nome: "Inter", punti: 0, falliSubiti: 0 },
+    { nome: "Milan", punti: 0, falliSubiti: 0 },
+    { nome: "Roma", punti: 0, falliSubiti: 0 },
+    { nome: "Napoli", punti: 0, falliSubiti: 0 }
+];
+// console.log(squadreDiCalcio);
 // Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+squadreDiCalcio.forEach(squadra => {
+        squadra.punti = getRndInteger(0,30)
+        squadra.falliSubiti = getRndInteger(30,200)
+});
+
+console.log(squadreDiCalcio);
+
+
+
 // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+onlyNameAndfails = []
+
+squadreDiCalcio.forEach(squadra => {
+    let {nome, falliSubiti} = squadra
+    nome = squadra.nome
+    falliSubiti = squadra.falliSubiti
+    onlyNameAndfails.push(nome+falliSubiti)
+    //un oggetto che 
+    return onlyNameAndfails
+});
+
+console.log(onlyNameAndfails);
